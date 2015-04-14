0222587264235c895b485d9240c0474943523287
eduardojonssen <eduardojonssen@gmail.com>
2015-04-14 10:09:12 -0300

    Adicionados arquivos perdidos

---
+++ PorquinhoDeOuro.Core/Log/AbstractLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PorquinhoDeOuro.Core.Utility;
+
+namespace PorquinhoDeOuro.Core.Log {
+
+    public abstract class AbstractLog : ILog {
+
+        public IConfigurationUtility ConfigurationUtility { get; set; }
+
+        public abstract void Save(string methodName, string type, object logData);
+    }
+}
---
+++ PorquinhoDeOuro.Core/Processor/SilverProcessor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PorquinhoDeOuro.Core.Processor {
+    public class SilverProcessor : AbstractProcessor {
+
+        public SilverProcessor() { }
+
+        public override string GetName() {
+            return this.GetType().Name;
+        }
+
+        public override IEnumerable<int> GetAvailableValues() {
+            return new int[] { 1000000, 50000 };
+        }
+    }
+}
